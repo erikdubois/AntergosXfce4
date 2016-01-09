e00cad0c779ab7042502238b64f0d268a00a61d1
erikdubois <erik.dubois@gmail.com>
2016-01-09 07:37:06 +0100

    Manual Backup @ Sat Jan  9 07:37:06 CET 2016

---
+++ 1_install_packer _v26.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#              `.-/::/-``
+#            .-/osssssssso/.               
+#           :osyysssssssyyys+-              
+#        `.+yyyysssssssssyyyyy+.           
+#       `/syyyyyssssssssssyyyyys-`         
+#      `/yhyyyyysss++ssosyyyyhhy/`         
+#     .ohhhyyyyso++/+oso+syy+shhhho.       
+#    .shhhhysoo++//+sss+++yyy+shhhhs.      
+#   -yhhhhs+++++++ossso+++yyys+ohhddy:     
+#  -yddhhyo+++++osyyss++++yyyyooyhdddy-    
+# .yddddhso++osyyyyys+++++yyhhsoshddddy`   
+#`odddddhyosyhyyyyyy++++++yhhhyosddddddo   
+#.dmdddddhhhhhhhyyyo+++++shhhhhohddddmmh.  
+#ddmmdddddhhhhhhhso++++++yhhhhhhdddddmmdy  
+#dmmmdddddddhhhyso++++++shhhhhddddddmmmmh  
+#-dmmmdddddddhhyso++++oshhhhdddddddmmmmd- 
+# .smmmmddddddddhhhhhhhhhdddddddddmmmms. 
+#   `+ydmmmdddddddddddddddddddmmmmdy/.     
+#      `.:+ooyyddddddddddddyyso+:.`
+#======================================================================================
+
+#======================================================================================
+#       
+# Author  : Erik Dubois at http://www.erikdubois.be
+# License : Distributed under the terms of GNU GPL version 2 or later
+# 
+# AS ALLWAYS, KNOW WHAT YOU ARE DOING.
+#======================================================================================
+
+# fstab for ssd 
+# defaults,noatime,discard
+# https://wiki.archlinux.org/index.php/Solid_State_Drives#Enable_TRIM_by_Mount_Flags
+# sudo fstrim -v / and /home
+
+# This has been dealt with in the release.
+
+
+# if you know what graphic driver to include you can do it here
+# but again not needed for many of us since the latest
+# linux kernels seem to support many graphicacl hardware.
+# Test out and if needed install any proprietary drivers from nvidia or ati.
+
+# sudo pacman -S nvidia-340xx
+
+
+        # A U R   H E L P E R #
+
+# Depending on your selection during installation this is already installed.
+
+
+# downloading yaourt to get packer (comes from archlinux.fr)
+# add these lines to the end of /etc/pacman.conf and update with sudo pacman -Syu
+# https://wiki.archlinux.org/index.php/Pacman
+# once I have yaourt I comment these lines out
+
+########################################
+#[archlinuxfr]
+#SigLevel = Never
+#Server = http://repo.archlinux.fr/$arch
+########################################
+
+# ANTERGOS use their own repo at 
+########################################
+#[antergos]
+#SigLevel = PackageRequired
+#Include = /etc/pacman.d/antergos-mirrorlist
+########################################
+# Server = http://mirrors.antergos.com/$repo/$arch
+########################################
+
+
+# downloading yaourt to get packer (comes from archlinux.fr)
+
+echo "################################################################"
+echo "yaourt is normally already installed"
+echo "################################################################"
+
+# sudo pacman -S yaourt
+
+# now we are downloading the aur helper I like
+
+echo "################################################################"
+echo "packer will be installed now"
+echo "################################################################"
+
+yaourt packer
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
+
