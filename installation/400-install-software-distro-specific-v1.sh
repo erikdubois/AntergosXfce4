3366cb4778cd18449677ac156374c299e493c42f
Erik Dubois <erik.dubois@gmail.com>
2017-02-23 09:55:30 +0100

    Comment :  on Thu 23 Feb 09:55:30 CET 2017

--- installation/400-install-software-distro-specific-v1.sh
+++ installation/400-install-software-distro-specific-v1.sh
@@ -20,7 +20,60 @@ echo "################################################################"
 
 sudo pacman -S noto-fonts --noconfirm --needed
 sudo pacman -S xfce4-whiskermenu-plugin --noconfirm
-sudo pacman -S mugshot --noconfirm
+
+
+
+
+
+
+package="mugshot"
+
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+	# Just checking if installation was successful
+	if pacman -Qi $package &> /dev/null; then
+	
+	echo "################################################################"
+	echo "#########  "$package" has been installed"
+	echo "################################################################"
+
+	else
+
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+	fi
+
+fi
 
 
 echo "################################################################"
